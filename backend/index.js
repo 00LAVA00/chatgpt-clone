0ab19dd424b926ac2b8b28df251e23f70b9b6bb3
00LAVA00 <aryanap037@gmail.com>
2024-09-09 00:12:14 +0530

    commit

--- backend/index.js
+++ backend/index.js
@@ -17,10 +17,8 @@ const __dirname = path.dirname(__filename);
 
 app.use(
   cors({
-    origin: process.env.CLIENT_URL,
-    credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
+    origin: process.env.CLIENT_URL, // Allow only your client URL
+    credentials: true, // Allow credentials
   })
 );
 
